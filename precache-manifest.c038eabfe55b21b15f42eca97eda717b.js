4e9c2f99a5beb15ae6285efbd68129cba5fbfe1d
Moath Alzaol <moath.alzaol@gmail.com>
2020-04-26 15:33:33 +0300

    Updates

---
+++ precache-manifest.c038eabfe55b21b15f42eca97eda717b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84958171d20c7415af25579309e8db39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b43e615ab732fc905b47",
+    "url": "/static/css/main.2686d530.chunk.css"
+  },
+  {
+    "revision": "c253da53607aa49c3c71",
+    "url": "/static/js/2.c4da8850.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c4da8850.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b43e615ab732fc905b47",
+    "url": "/static/js/main.c150c078.chunk.js"
+  },
+  {
+    "revision": "d81dee2517614a12cf6a",
+    "url": "/static/js/runtime-main.6284f926.js"
+  }
+]);
